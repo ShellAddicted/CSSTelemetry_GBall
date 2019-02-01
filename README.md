e4cbd1728aa0a71989274d39aa4324291c9714e2
ShellAddicted <shelladdicted@gmail.com>
2019-02-01 18:36:54 +0100

    Code cleanup, disabled IMU usage (Deprecated), minified version available, and user input in DEMO

--- README.md
+++ README.md
@@ -4,29 +4,47 @@
 ![screenshot](https://github.com/ShellAddicted/CSSTelemetry_GBall/blob/master/screenshot.jpg)
 
 
-# Quickstart
-HTML:
-```html
-<link rel="stylesheet" type="text/css" href="csstelemetry-gball.css" />
-<div id="ball1" class="csstelemetry-gball"style="height: 300px; width: 300px;"></div>
-<script src="js/csstelemetry-gball.js"></script>
-```
-Other CSS classes:
+## DEMO
+https://shelladdicted.github.io/CSSTelemetry_GBall/
 
-<b>Align</b>
+# Quickstart
+## HTML:
+```html
+<link rel="stylesheet" type="text/css" href="dist/csstelemetry-gball.min.css" />
+<div id="ball1" class="csstelemetry-gball csstelemetry-gball-left" style="height: 300px; width: 300px;"></div>
+<script src="dist/csstelemetry-gball.min.js"></script>
+```
+#### Alignment options
 - ```csstelemetry-gball-left```
 - ```csstelemetry-gball-center```
 - ```csstelemetry-gball-right```
 
-JS:
+## JS:
 ```javascript
-// ID of <div>, cursor diameter, max G value
-ball = new GBall("ball1",4,8);
-// show a Force -> G values (x,y)
+/*
+  GBall(DOMElement, cursorSize = 4, maxGValue = 8, steps = 4, dashed = true)
+
+  DOMElement: DOM Element reserved for GBall
+  cursorSize: (int)size of force indicator
+  maxGValue: (float)maximum representable value
+  steps: (int) number of steps between -maxGValue && +maxGValue (more steps =
+  more resolution) dashed: (bool) alternates solid circle and dashed circles
+*/
+ball = new GBall(document.getElementById("ball1"),4,8);
+/*
+  showForce(xAxis, yAxis)
+
+  xAxis: (float) force along xAxis (unit: G)
+  yAxis: (float) force along yAxis (unit: G)
+
+  Examples:
+    ball.showForce(2,1); // 2g (x) ; 1g(y)
+    ball.showForce(ms2tog(12), ms2tog(13)) //12m/s² (x) ; 13mm/s² (y)
+*/
 ball.showForce(2,1); // 2g (x) ; 1g(y)
-//Alternative:
-ball.showForce(ms2tog(12),ms2tog(13))) //12m/s² (x) ; 13mm/s² (y)
 ```
 
-## DEMO
-https://shelladdicted.github.io/CSSTelemetry_GBall/
+#### Unit conversion helpers
+- ```const: G = 9.80665 (m/s²)```
+- ```function: ms2tog(m/s²) -> g```
+- ```function: gtoms2(g) -> m/s²```
--- js/csstelemetry-gball.js
+++ js/csstelemetry-gball.js
@@ -1,58 +1,77 @@
-/*Copyright (c) 2018 ShellAddicted <ShellAddicted@gmail.com> - MIT License*/
+/*Copyright (c) 2019 ShellAddicted <ShellAddicted@gmail.com> - MIT License*/
 const G = 9.80665;
-
+// eslint-disable-next-line no-unused-vars
 function ms2tog(ms2) {
-	/*Convert m/s² to g*/
-	return ms2 * (1 / G);
+  /*Convert m/s² to g*/
+  return ms2 / G;
 }
-
+// eslint-disable-next-line no-unused-vars
 function gtoms2(g) {
-	/*Convert g to m/s²*/
-	return g * G;
+  /*Convert g to m/s²*/
+  return g * G;
 }
 
+// eslint-disable-next-line no-unused-vars
 class GBall {
-	constructor(id, size = 4, scale = 8) {
-		this.size = size;
-		this.scale = scale;
-		this.ball = document.getElementById(id);
-		this.cursor = document.createElement("div");
-		this.cursor.classList.add("csstelemetry-gball-cursor");
-		this.cursor.style.height = size + "%";
-		this.cursor.style.width = size + "%";
-		this.cursor.style.margin = -(size / 2) + "% 0 0 " + -(size / 2) + "%";
-		this.ball.appendChild(this.cursor);
-		/*Scale*/
-		const steps = 3
-		for (let i = steps; i > 0; i--) {
-			let currentStep = (25 * i);
-			let grid = document.createElement("div");
-			grid.classList.add("csstelemetry-gball");
-			grid.classList.add("csstelemetry-gball-rect");
-			grid.style.width = grid.style.height = currentStep + "%";
-			grid.style.margin = -(currentStep / 2) + "% 0 0 " + -(currentStep / 2) + "%";
-			let span = document.createElement("span");
-			span.innerText = ((currentStep / 100) * scale) + "G";
-			grid.appendChild(span);
-			if (i % 2 != 0) {
-				grid.style.borderStyle = "dashed";
-			}
-			this.ball.appendChild(grid);
-		}
-		let span = document.createElement("span");
-		span.innerText = Math.trunc(this.scale) + "G";
-		this.ball.appendChild(span);
+  constructor(DOMElement, cursorSize = 4, maxGValue = 8, steps = 4, dashed = true) {
+    /*
+    GBall(DOMElement, cursorSize = 4, maxGValue = 8, steps = 4, dashed = true)
 
-	}
+    DOMElement: DOM Element reserved for GBall
+    cursorSize: (int)size of force indicator
+    maxGValue: (float)maximum representable value
+    steps: (int) number of steps between -maxGValue && +maxGValue (more steps =
+    more resolution) dashed: (bool) alternates solid circle and dashed circles
+    */
+    this.cursorSize = cursorSize;
+    this.maxGValue = maxGValue;
+    this.ball = DOMElement;
+    this.cursor = document.createElement('div');
+    this.cursor.classList.add('csstelemetry-gball-cursor');
+    this.cursor.style.height = this.cursorSize + '%';
+    this.cursor.style.width = this.cursorSize + '%';
+    this.cursor.style.margin = -(this.cursorSize / 2) + '% 0 0 ' + -(this.cursorSize / 2) + '%';
+    this.ball.appendChild(this.cursor);
+    // steps-1 -> don't draw the last ring because it's already there
+    for (let i = steps-1; i > 0; i--) {
+      let currentStep = ((100 / steps) * i);
+      let grid = document.createElement('div');
+      grid.classList.add('csstelemetry-gball');
+      grid.classList.add('csstelemetry-gball-rect');
+      grid.style.width = grid.style.height = currentStep + '%';
+      grid.style.margin = -(currentStep / 2) + '% 0 0 ' + -(currentStep / 2) + '%';
+      let span = document.createElement('span');
+      span.innerText = ((currentStep / 100) * this.maxGValue).toFixed(2) + 'G';
+      grid.appendChild(span);
+      if (dashed && (i % 2 != 0)) {
+        grid.style.borderStyle = 'dashed';
+      }
+      this.ball.appendChild(grid);
+    }
+    let span = document.createElement('span');
+    span.innerText = this.maxGValue.toFixed(2) + 'G';
+    this.ball.appendChild(span);
+  }
 
-	showForce(xAxis, yAxis) {
-		let angle = Math.atan(yAxis / xAxis);
-		let maxY = Math.sin(angle) * this.scale;
-		let maxX = Math.cos(angle) * this.scale;
-		let res = [(Math.abs(xAxis) > Math.abs(maxX)) ? maxX : xAxis, (Math.abs(yAxis) > Math.abs(maxY)) ? maxY : yAxis];
-		this.cursor.style.left = 50 + ((50 - (this.size / 2)) * res[0]) / this.scale + "%";
-		this.cursor.style.top = 50 + ((50 - (this.size / 2)) * res[1]) / this.scale + "%";
-	}
+  showForce(xAxis, yAxis) {
+    /*
+    showForce(xAxis, yAxis)
+
+    xAxis: (float) force along xAxis (unit: G)
+    yAxis: (float) force along yAxis (unit: G)
+    */
+    let angle = Math.atan(yAxis / xAxis);
+    let maxY = Math.sin(angle) * this.maxGValue;
+    let maxX = Math.cos(angle) * this.maxGValue;
+    let res = [
+      (Math.abs(xAxis) > Math.abs(maxX)) ? maxX : xAxis,
+      (Math.abs(yAxis) > Math.abs(maxY)) ? maxY : yAxis
+    ];
+    this.cursor.style.left =
+        50 + ((50 - (this.cursorSize / 2)) * res[0]) / this.maxGValue + '%';
+    this.cursor.style.top =
+        50 + ((50 - (this.cursorSize / 2)) * res[1]) / this.maxGValue + '%';
+  }
 }
 
 /*
@@ -60,16 +79,15 @@ Example:
 
 // HTML
 <link rel="stylesheet" type="text/css" href="csstelemetry-gball.css" />
-<div id="ball1" class="csstelemetry-gball" style="height: 200px; width:200px;"></div>
-<script src="csstelemetry-gball.js"></script>
+<div id="ball1" class="csstelemetry-gball" style="height: 200px;
+width:200px;"></div> <script src="csstelemetry-gball.js"></script>
 
 //JS
-ball = new GBall("ball1");
+ball = new GBall(document.getElementById("ball1"));
 
 ball.showForce(2,1); // 2g (x) ; 1g(y)
-OR 
+OR
 ball.showForce(ms2tog(12),ms2tog(13)) //12m/s² (x) ; 13mm/s² (y)
 
-// Done! 
+// Done!
 */
-
